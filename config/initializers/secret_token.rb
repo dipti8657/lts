c12ee37c7e0e631b8daed68bb31b7ec4c633815f
Meghali <meghali.dhoble@gmail.com>
2012-05-16 10:13:11 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CuberootLts::Application.config.secret_token = '6d82e99fbdd5e4446471f620993845d34374e7a18fc98aabfac364d37ac2d433ec684ddc6130ed9d5edab51d986ba148cd6956c32fe8970d16494d1a040f2688'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CuberootLts::Application.config.session_store :cookie_store, key: '_cuberoot_lts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CuberootLts::Application.config.session_store :active_record_store
